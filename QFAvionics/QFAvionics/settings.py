4179b9acce01b2e60f860774e9c6ee57abd1fd67
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-10-21 10:54:21 +0530

    "Updated header context, added context processor to settings, and modified product template"

--- QFAvionics/QFAvionics/settings.py
+++ QFAvionics/QFAvionics/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'QFAvionics.header_context.header_options'
             ],
         },
     },
