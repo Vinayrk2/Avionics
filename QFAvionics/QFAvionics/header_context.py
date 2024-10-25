616cf61b5029aa1b03ba2b08154af4abb8faed15
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-10-25 12:26:56 +0530

    "Updated header context to return Service objects, modified URLs and views for service pages, and refactored search result template and view to display products with images and categories."

--- QFAvionics/QFAvionics/header_context.py
+++ QFAvionics/QFAvionics/header_context.py
@@ -4,7 +4,7 @@ from additional_option.models import Service, Link
 def header_options(request):
     options = {
         "categories": [category.name for category in Category.objects.all()],
-        "services" : [service.name for service in Service.objects.all()],
+        "services" : [service for service in Service.objects.all()],
         "links": Link.objects.all(),
     }
     
--- QFAvionics/additional_option/views.py
+++ QFAvionics/additional_option/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
+def service(request, id):
+    service = Service.objects.filter(id=id).first()
+    return render(request, 'service.html', {'service':service})
