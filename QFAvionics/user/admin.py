ea180e77d9ecf15bd08c3f2f38dc8276576901b0
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-11-08 22:48:26 +0530

    ccvd

--- QFAvionics/user/admin.py
+++ QFAvionics/user/admin.py
@@ -5,4 +5,5 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'username')
-
+    readonly_fields = ('date_joined', 'last_login', 'password')
+    list_filter = ('is_active', 'is_staff')
