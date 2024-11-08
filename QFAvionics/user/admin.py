4ce886a84c0a238351a7c4abede81d06e44a8fbe
VINAY-KOSHTI <vinaykoshti02@gmail.com>
2024-11-08 22:58:15 +0530

    Added ordering to CustomUserAdmin in admin.py

--- QFAvionics/user/admin.py
+++ QFAvionics/user/admin.py
@@ -7,3 +7,4 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'username')
     readonly_fields = ('date_joined', 'last_login', 'password')
     list_filter = ('is_active', 'is_staff')
+    ordering = ('-date_joined',)
